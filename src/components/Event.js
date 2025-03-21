c3aad1243ea50a469eb511323da4eac171487d69
manas <manasjaincode@gmail.com>
2025-03-21 21:14:07 +0530

    made changes

--- src/components/Event.js
+++ src/components/Event.js
@@ -27,8 +27,8 @@ function Events() {
           <div className="flex flex-col lg:flex-row justify-between gap-8">
             <div>
               <h3 className="text-3xl font-bold text-gray-400 mb-2">Melange 2025</h3>
-              <p className="text-xl text-gray-500 mb-4">Indore's Biggest Fashion show organized during Moonstone Fest</p>
-            </div>
+              <p className="text-xl text-gray-500 mb-4">Indore&apos;s Biggest Fashion show organized during Moonstone Fest</p>
+              </div>
             <div className="space-y-4">
               <p className="text-gray-500">Date : 28 March 2025</p>
               <p className="text-gray-500">Venue : Medicaps University Indore</p>
